ba769563eabc8d2e66a3baad236b2a12660dafc0
Gunank <gunanksood1222@gmail.com>
2018-02-02 00:21:27 +0530

    updated the database schema

--- table-schema.sql
+++ table-schema.sql
@@ -1,7 +1,7 @@
 -- sqlite3 content_table.db < table-schema.sql
 
 drop table if exists content_table;
-create content_table content_table (
+create table content_table (
   id integer primary key autoincrement,
   title text not null,
   content_text text not null
